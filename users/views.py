10ba546e791189509d614799c658856204ed665d
ashwini <ashwini@localhost.localdomain>
2021-01-25 06:49:25 -0500

    Updated Blog-project added

---
+++ users/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm,ProfileUpdateForm,UserUpdateForm
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            fname = form.cleaned_data.get('first_name')
+            messages.success(request,f'Welcome to BlogBuzz, {fname.capitalize()}!!! Login to start blogging.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request,f'Profile has been updated successfully.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request,'users/profile.html',context)
+
+def main_home(request):
+    return render(request,'users/main_home.html')
